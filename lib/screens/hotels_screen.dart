ca582451927cbd70752fb2e0ecdd9690c6e8123f
chrxstabel <chrxstabel@gmail.com>
2023-08-08 02:29:18 +0800

    initial commit

--- lib/screens/hotels_screen.dart
+++ lib/screens/hotels_screen.dart
@@ -72,7 +72,7 @@ class _HotelsScreenState extends State<HotelsScreen> {
                 controller: _searchController,
                 onChanged: _filterHotels,
                 decoration: InputDecoration(
-                  hintText: 'Search hotels...',
+                  hintText: 'Explore hotels...',
                   prefixIcon: Icon(Icons.search),
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(10),
@@ -80,84 +80,126 @@ class _HotelsScreenState extends State<HotelsScreen> {
                 ),
               ),
             ),
-            Expanded(
-              child: ListView.builder(
-                padding: EdgeInsets.only(top: 10.0, bottom: 15.0),
-                itemCount: _filteredHotels.length,
-                itemBuilder: (BuildContext context, int index) {
-                  Hotel hotel = _filteredHotels[index];
-                  return Stack(
-                    children: <Widget>[
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/hotelDetail',
-                              arguments: hotel);
-                        },
-                        child: Container(
-                          margin: EdgeInsets.fromLTRB(25.0, 5.0, 20.0, 5.0),
-                          height: 170.0,
-                          width: double.infinity,
-                          decoration: BoxDecoration(
-                            color: Colors.white,
-                            borderRadius: BorderRadius.circular(20.0),
-                          ),
-                          child: Padding(
-                            padding:
-                                EdgeInsets.fromLTRB(100.0, 20.0, 20.0, 20.0),
-                            child: Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: <Widget>[
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.end,
-                                  crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: <Widget>[
-                                    Container(
-                                      width: 140.0,
-                                      child: Text(
-                                        hotel.name,
-                                        style: TextStyle(
-                                          fontSize: 15.0,
-                                          fontWeight: FontWeight.w600,
+            Container(
+              height: 400,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: SizedBox(
+                  width: MediaQuery.of(context).size.width * 2,
+                  child: DefaultTabController(
+                    length: 2,
+                    child: Column(
+                      children: [
+                        TabBar(
+                          tabs: [
+                            Tab(text: 'All'),
+                            Tab(text: 'Liked'),
+                          ],
+                        ),
+                        Expanded(
+                          child: TabBarView(
+                            physics: NeverScrollableScrollPhysics(),
+                            children: [
+                              ListView.builder(
+                                padding:
+                                    EdgeInsets.only(top: 10.0, bottom: 15.0),
+                                itemCount: _filteredHotels.length,
+                                itemBuilder: (BuildContext context, int index) {
+                                  Hotel hotel = _filteredHotels[index];
+                                  return Card(
+                                    margin: EdgeInsets.only(right: 22.0),
+                                    clipBehavior: Clip.antiAlias,
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(30.0),
+                                    ),
+                                    elevation: 0.0,
+                                    child: InkWell(
+                                      onTap: () {
+                                        Navigator.pushNamed(
+                                            context, '/hotelDetail',
+                                            arguments: hotel);
+                                      },
+                                      child: Container(
+                                        decoration: BoxDecoration(
+                                            image: DecorationImage(
+                                          image:
+                                              NetworkImage(hotel.propertyImage),
+                                          fit: BoxFit.cover,
+                                          scale: 2.0,
+                                        )),
+                                        width: 200.0,
+                                        child: Padding(
+                                          padding: EdgeInsets.all(12.0),
+                                          child: Column(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.start,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.start,
+                                            children: [
+                                              Row(
+                                                children: [
+                                                  Icon(
+                                                    Icons.star,
+                                                    color: Color(0xFFFE8C68),
+                                                  ),
+                                                  Expanded(
+                                                    child: Column(
+                                                      mainAxisAlignment:
+                                                          MainAxisAlignment.end,
+                                                      crossAxisAlignment:
+                                                          CrossAxisAlignment
+                                                              .start,
+                                                      children: [
+                                                        Text(
+                                                          hotel.name,
+                                                          style: TextStyle(
+                                                            color: Colors.white,
+                                                            fontSize: 20.0,
+                                                            fontWeight:
+                                                                FontWeight.w600,
+                                                          ),
+                                                        ),
+                                                        SizedBox(
+                                                          height: 6.0,
+                                                        ),
+                                                        Text(
+                                                          '\$${hotel.price}',
+                                                          style: TextStyle(
+                                                            color: Colors.white,
+                                                            fontSize: 18.0,
+                                                            fontWeight:
+                                                                FontWeight.w400,
+                                                          ),
+                                                        ),
+                                                        Text(
+                                                          'Review Score ${hotel.reviewScore}',
+                                                          style: TextStyle(
+                                                            color: Colors.white,
+                                                            fontSize: 18.0,
+                                                            fontWeight:
+                                                                FontWeight.w400,
+                                                          ),
+                                                        ),
+                                                      ],
+                                                    ),
+                                                  ),
+                                                ],
+                                              )
+                                            ],
+                                          ),
                                         ),
-                                        overflow: TextOverflow.ellipsis,
-                                        maxLines: 2,
                                       ),
                                     ),
-                                    Text(
-                                      '\$${hotel.price}',
-                                      style: TextStyle(
-                                          fontSize: 15.0,
-                                          fontWeight: FontWeight.w600),
-                                    ),
-                                  ],
-                                ),
-                                Align(
-                                    alignment: Alignment.center,
-                                    child: Text(
-                                        'Review Score ${hotel.reviewScore}')),
-                                SizedBox(height: 20.0)
-                              ],
-                            ),
+                                  );
+                                },
+                              ),
+                            ],
                           ),
                         ),
-                      ),
-                      Positioned(
-                        left: 30.0,
-                        top: 15.0,
-                        bottom: 15.0,
-                        child: ClipRRect(
-                          borderRadius: BorderRadius.circular(20.0),
-                          child: Image.network(
-                            hotel.propertyImage,
-                            width: 140.0,
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                    ],
-                  );
-                },
+                      ],
+                    ),
+                  ),
+                ),
               ),
             ),
           ],
