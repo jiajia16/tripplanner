e93df5c8d6dfafac1e42bfa5ab45909cd6690291
chrxstabel <chrxstabel@gmail.com>
2023-08-07 23:29:48 +0800

    initial commit

--- lib/screens/hotels_screen.dart
+++ lib/screens/hotels_screen.dart
@@ -12,13 +12,25 @@ class HotelsScreen extends StatefulWidget {
   State<HotelsScreen> createState() => _HotelsScreenState();
 }
 
+final List<String> hotels = <String>[
+  'id',
+  'name',
+  'propertyImage',
+  'price',
+  'reviewScore'
+];
+
 class _HotelsScreenState extends State<HotelsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey,
       appBar: AppBar(
-        backgroundColor: Colors.purpleAccent,
-        title: const Text('Hotels'),
+        backgroundColor: Colors.cyanAccent,
+        title: const Text(
+          'Hotels',
+          style: TextStyle(color: Colors.black),
+        ),
       ),
       bottomNavigationBar: MyBottomNavigationBar(selectedIndexNavBar: 1),
       body: SafeArea(
@@ -27,27 +39,81 @@ class _HotelsScreenState extends State<HotelsScreen> {
           builder: (context, snapshot) {
             if (snapshot.hasData) {
               return ListView.builder(
+                padding: EdgeInsets.only(top: 10.0, bottom: 15.0),
                 itemCount: snapshot.data!.length,
-                itemBuilder: (context, index) {
+                itemBuilder: (BuildContext context, int index) {
                   Hotel hotel = snapshot.data![index];
-                  return ListTile(
-                    onTap: () {
-                      Navigator.pushNamed(context, '/hotelDetail',
-                          arguments: hotel);
-                    },
-                    leading: Image.network(hotel.propertyImage),
-                    title: Text(hotel.name),
-                    subtitle: Row(
-                      children: [
-                        Icon(
-                          Icons.star,
-                          color: Colors.purple,
-                          size: 18,
+
+                  return Stack(
+                    children: <Widget>[
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, '/hotelDetail',
+                              arguments: hotel);
+                        },
+                        child: Container(
+                          margin: EdgeInsets.fromLTRB(25.0, 5.0, 20.0, 5.0),
+                          height: 170.0,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(20.0),
+                          ),
+                          child: Padding(
+                            padding:
+                                EdgeInsets.fromLTRB(100.0, 20.0, 20.0, 20.0),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  crossAxisAlignment: CrossAxisAlignment.end,
+                                  children: <Widget>[
+                                    Container(
+                                      width: 140.0,
+                                      child: Text(
+                                        hotel.name,
+                                        style: TextStyle(
+                                          fontSize: 15.0,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                        overflow: TextOverflow.ellipsis,
+                                        maxLines: 2,
+                                      ),
+                                    ),
+                                    Text(
+                                      '\$${hotel.price}',
+                                      style: TextStyle(
+                                          fontSize: 15.0,
+                                          fontWeight: FontWeight.w600),
+                                    ),
+                                  ],
+                                ),
+                                Align(
+                                    alignment: Alignment.center,
+                                    child: Text(
+                                        'Review Score ${hotel.reviewScore}')),
+                                SizedBox(height: 20.0)
+                              ],
+                            ),
+                          ),
                         ),
-                        Text('${hotel.reviewScore}'),
-                      ],
-                    ),
-                    trailing: Text(hotel.price),
+                      ),
+                      Positioned(
+                        left: 30.0,
+                        top: 15.0,
+                        bottom: 15.0,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(20.0),
+                          child: Image.network(
+                            hotel.propertyImage,
+                            width: 140.0,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    ],
                   );
                 },
               );
